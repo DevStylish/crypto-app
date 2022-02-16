ccdd431be4bd1c4df682cd9f2dd786d58d41bca7
DevStylish <moisesmarper10@gmail.com>
2022-02-16 19:53:48 +0100

    Restauración del cors a enviroment

--- src/app/service/crypto.service.ts
+++ src/app/service/crypto.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from 'src/environments/environment';
 import { CryptoCoin } from '../crypto/interfaces/cryptocoin.interface';
 import { Observable } from 'rxjs';
 
@@ -9,19 +10,18 @@ import { Observable } from 'rxjs';
 })
 export class CryptoService {
 
-  private apiUrl: string = "https://cors-anywhere.herokuapp.com/https://api.nomics.com/v1/currencies/ticker";
   private apiKey: string = "a6d70796dd2837f0fedc3e6a57950c982e2a0a31";
 
   constructor(private http: HttpClient) { }
 
   buscarCrypto(cryptoText: string): Observable<CryptoCoin[]>{
-    let url = `${this.apiUrl}?key=${this.apiKey}&ids=${cryptoText}`;
+    let url = `${environment.apiUrl}?key=${this.apiKey}&ids=${cryptoText}`;
 
     return this.http.get<CryptoCoin[]>(url);
   }
 
   obtenerCryptoRanking(){
-    let url = `${this.apiUrl}?key=${this.apiKey}&status=active&sort=rank&per-page=10&page=1`;
+    let url = `${environment.apiUrl}?key=${this.apiKey}&status=active&sort=rank&per-page=10&page=1`;
 
     return this.http.get<CryptoCoin[]>(url);
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://cors-anywhere.herokuapp.com/https://api.nomics.com/v1/currencies/ticker'
 };
